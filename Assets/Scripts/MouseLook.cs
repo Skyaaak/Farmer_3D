c562f52a40dcdd3cbf8efc1cce254ac7b388e47b
le-coatFrederic <lecoatfred@gmail.com>
2024-11-26 14:31:26 +0100

    Amélioration du système et correction de l'erreur de vitesse horizontale et de saccade.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField] private GameInput gameInput;
-    [SerializeField] private float mouseSensitivity = 100f;
-    [SerializeField] private Transform playerBody;
 
     private float xRotation = 0f;
 
@@ -21,14 +19,11 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         Vector2 mouseDirection = gameInput.GetLookVectorNormalized();
-        float mouseX = mouseDirection.x * mouseSensitivity * Time.deltaTime;
-        float mouseY = mouseDirection.y * mouseSensitivity * Time.deltaTime;
+        float mouseY = mouseDirection.y * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -70, 80f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/ToolSlot.cs
+++ Assets/Scripts/ToolSlot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ToolSlot : MonoBehaviour
+public class ToolSlot
 {
     public ItemData item;
     public Image itemVisual;
