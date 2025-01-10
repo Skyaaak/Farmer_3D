900588b306495a8020d57e6d3ad31f866c7950ff
Cléa <jan.clea20@gmail.com>
2025-01-10 19:22:14 +0100

    début d'un menu plus propre

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -12,9 +12,13 @@ public class MenuScript : MonoBehaviour
         
         if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth / 2, Screen.height / 2 - buttonHeight / 2, buttonWidth, buttonHeight), "Jouer"))
         {
-            // Load the game scene when the button is clicked
-            //Application.LoadLevel("SampleScene");
+            // Charger la scène du jeu lorsque le bouton est cliqué.
             SceneManager.LoadScene("SampleScene");
         }
+
+        // Créer 3 boutons : jouer, commandes, quitter
+        // Jouer lance la scène SampleScene (peut etre la renommer ?)
+        // Commande lance la scène CommandesScènes que j'implémenterai
+        // Quitter quitte le jeu
     }
 }
